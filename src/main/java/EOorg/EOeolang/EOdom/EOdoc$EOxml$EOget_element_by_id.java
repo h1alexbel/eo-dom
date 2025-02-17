664657cb5ed072e910795c8ddbd3f9997185d412
h1alexbel <h1alexbelx@gmail.com>
2025-02-17 21:01:33 +0300

    feat(#48): atom

---
+++ src/main/java/EOorg/EOeolang/EOdom/EOdoc$EOxml$EOget_element_by_id.java
@@ -0,0 +1,42 @@
+package EOorg.EOeolang.EOdom;
+
+import org.eolang.AtVoid;
+import org.eolang.Atom;
+import org.eolang.Attr;
+import org.eolang.Data;
+import org.eolang.Dataized;
+import org.eolang.PhDefault;
+import org.eolang.Phi;
+import org.eolang.XmirObject;
+
+/**
+ * Retrieval of the element with specified `id` property.
+ *
+ * @since 0.0.0
+ */
+@XmirObject(oname = "doc.xml.get-element-by-id")
+public final class EOdoc$EOxml$EOget_element_by_id extends PhDefault implements Atom {
+
+    /**
+     * Ctor.
+     */
+    public EOdoc$EOxml$EOget_element_by_id() {
+        this.add("identifier", new AtVoid("identifier"));
+    }
+
+    @Override
+    public Phi lambda() throws XmlParseException {
+        final XmlNode.Default serialized = new XmlNode.Default(
+            new Dataized(this.take(Attr.RHO).take("serialized")).asString()
+        );
+        System.out.println(serialized.asString());
+        final String found = new XmlNode.Default(
+            serialized
+                .doc()
+                .getElementById(new Dataized(this.take("identifier")).asString())
+        ).asString();
+        final Phi element = Phi.Φ.take("org.eolang.dom.element");
+        element.put("xml", new Data.ToPhi(found.getBytes()));
+        return element;
+    }
+}
